671872567f3f6939fa9904d5a51f8a01f9ec1f45
daveKidd <david.kidd@lambdaschool.com>
2021-02-24 11:18:54 -0700

    ketchup

--- car/car.js
+++ car/car.js
@@ -1,1 +1,7 @@
-// Build a Car class!
+class Car {
+    constructor(make){
+        this.make = make
+    }
+}
+
+module.exports = Car
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,3 +1,38 @@
+const Car = require("./car.js")
+
+//TDD - Test driven development
+//1 - Create the test, make sure it fails
+//2 - Get it passing with the least
+//amount of code
+//3 - Refactor it - make it pretty/easy to read
+
+
 it("works",()=>{
-    expect(4).toBe(4)         //eslint-disable-line
+    expect(3).toBeGreaterThan(2)    //eslint-disable-line
 })
+
+test("toEqual and toBe", ()=>{
+    let a = {}
+    let b = {}
+    let c = a
+    //expect(a).toBe(b)
+    //expect(a).toEqual(b)
+    expect(a).toBe(c)
+})
+
+describe("car class",()=>{
+    let odyssey
+    beforeEach(()=>{
+        odyssey = new Car("honda","odyssey")
+    })
+    it("exists", ()=>{
+        expect(Car).toBeDefined()
+    })
+    it("creates car instances",()=>{        
+        expect(odyssey).toBeInstanceOf(Car)
+    })
+    it("cars have a make prop",()=>{
+        expect(odyssey).toHaveProperty("make")
+    })
+})
+
